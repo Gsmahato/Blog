d22fe49d1b8ef728dfd2369d0639665c8439ef45
unknown <gsmahato>
2023-10-06 03:36:38 +0545

    change

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import React from "react";
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import ganesha from "../public/ganesha.jpeg";
+import image1 from "../public/image1.jpeg";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.container}>
+          <div className={styles.site_row}>
+            <div className={styles.left_card1}>
+              <h3>Paragraph</h3>
+              <Link href="/">
+                <Image src={image1} width={350} height={234} alt="" />
+                <div className={styles.blog_content_body2}>
+                  <div className={styles.half_text3}>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Non harum ad animi voluptas quia ea quisquam fuga maxime
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className={styles.left_card2}>
+              <h3>Latest Posts</h3>
+              <ul>
+                <li>
+                  <Link href=''>
+                    <Image src={image1} width={90} height={60} alt="images"/>
+                    <div className={styles.blog_content_body1}>
+                    <div className={styles.half_text2}>
+                      <div className={styles.post_meta2}>
+                        <span className={styles.post_category2}>Category</span>
+                        <span className={styles.post_date2}>2023/10/4</span>
+                      </div>
+                      <h2>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit.
+                      </h2>
+                    </div>
+                  </div>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+      Footer
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/MoreBlog.js
+++ components/MoreBlog.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from "@/styles/Home.module.css"
+import React from "react";
+import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import ganesha from "../public/ganesha.jpeg";
@@ -10,7 +10,7 @@ import image4 from "../public/image4.jpg";
 const MoreBlog = () => {
   return (
     <>
-    <div className={styles.moreblog_section}>
+      <div className={styles.moreblog_section}>
         <div className={styles.container}>
           <div className={styles.headlines}>
             <div className={styles.headlines_name}>
@@ -19,93 +19,83 @@ const MoreBlog = () => {
           </div>
           <div className={styles.blog_enteries}>
             <div className={styles.left_col}>
-              <div className={styles.left_row}>
-                <div className={styles.left_card}>
-                  <Link href="/">
-                    <Image src={image1} width={350} height={234} alt="" />
-                    <div className={styles.blog_content_body}>
-                      <div className={styles.half_text2}>
-                        <div className={styles.post_meta2}>
-                          <span className={styles.post_category2}>
-                            Category
-                          </span>
-                          <span className={styles.post_date2}>2023/10/4</span>
-                        </div>
-                        <h2>
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit.
-                        </h2>
+              <div className={styles.horizontal_card}>
+                <Link href="/">
+                  <Image src={image1} width={200} height={171} alt="" />
+                  <div className={styles.blog_content_body1}>
+                    <div className={styles.half_text2}>
+                      <div className={styles.post_meta2}>
+                        <span className={styles.post_category2}>Category</span>
+                        <span className={styles.post_date2}>2023/10/4</span>
                       </div>
+                      <h2>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit.
+                      </h2>
                     </div>
-                  </Link>
-                </div>
-                <div className={styles.left_card}>
-                  <Link href="/">
-                    <Image src={image2} width={350} height={234} alt="" />
-                    <div className={styles.blog_content_body}>
-                      <div className={styles.half_text2}>
-                        <div className={styles.post_meta2}>
-                          <span className={styles.post_category2}>
-                            Category
-                          </span>
-                          <span className={styles.post_date2}>2023/10/4</span>
-                        </div>
-                        <h2>
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit.
-                        </h2>
+                  </div>
+                </Link>
+              </div>
+              <div className={styles.horizontal_card}>
+                <Link href="/">
+                  <Image src={image1} width={200} height={171} alt="" />
+                  <div className={styles.blog_content_body1}>
+                    <div className={styles.half_text2}>
+                      <div className={styles.post_meta2}>
+                        <span className={styles.post_category2}>Category</span>
+                        <span className={styles.post_date2}>2023/10/4</span>
                       </div>
+                      <h2>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit.
+                      </h2>
                     </div>
-                  </Link>
-                </div>
-                <div className={styles.left_card}>
-                  <Link href="/">
-                    <Image src={image3} width={350} height={234} alt="" />
-                    <div className={styles.blog_content_body}>
-                      <div className={styles.half_text2}>
-                        <div className={styles.post_meta2}>
-                          <span className={styles.post_category2}>
-                            Category
-                          </span>
-                          <span className={styles.post_date2}>2023/10/4</span>
-                        </div>
-                        <h2>
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Non harum ad animi voluptas quia ea quisquam
-                          fuga maxime
-                        </h2>
+                  </div>
+                </Link>
+              </div>
+              <div className={styles.horizontal_card}>
+                <Link href="/">
+                  <Image src={image1} width={200} height={171} alt="" />
+                  <div className={styles.blog_content_body1}>
+                    <div className={styles.half_text2}>
+                      <div className={styles.post_meta2}>
+                        <span className={styles.post_category2}>Category</span>
+                        <span className={styles.post_date2}>2023/10/4</span>
                       </div>
+                      <h2>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Non harum ad animi voluptas quia ea quisquam fuga
+                        maxime
+                      </h2>
                     </div>
-                  </Link>
-                </div>
-                <div className={styles.left_card}>
-                  <Link href="/">
-                    <Image src={image4} width={350} height={234} alt="" />
-                    <div className={styles.blog_content_body}>
-                      <div className={styles.half_text2}>
-                        <div className={styles.post_meta2}>
-                          <span className={styles.post_category2}>
-                            Category
-                          </span>
-                          <span className={styles.post_date2}>2023/10/4</span>
-                        </div>
-                        <h2>
-                          Lorem ipsum dolor, sit amet consectetur adipisicing
-                          elit. Non harum ad animi voluptas quia ea quisquam
-                          fuga maxime
-                        </h2>
+                  </div>
+                </Link>
+              </div>
+              <div className={styles.horizontal_card}>
+                <Link href="/">
+                  <Image src={ganesha} width={200} height={171} alt="" />
+                  <div className={styles.blog_content_body1}>
+                    <div className={styles.half_text2}>
+                      <div className={styles.post_meta2}>
+                        <span className={styles.post_category2}>Category</span>
+                        <span className={styles.post_date2}>2023/10/4</span>
                       </div>
+                      <h2>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Non harum ad animi voluptas quia ea quisquam fuga
+                        maxime
+                      </h2>
                     </div>
-                  </Link>
-                </div>
+                  </div>
+                </Link>
               </div>
             </div>
             <div className={styles.right_col}></div>
           </div>
         </div>
       </div>
-    MoreBlog</>
-  )
-}
+    </>
+  );
+};
 
-export default MoreBlog
+export default MoreBlog;
